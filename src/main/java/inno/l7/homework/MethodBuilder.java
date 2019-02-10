3c1431514bb7828dbae9b535dfa00207b7bacf0e
Anton Tsapin <sheog.k@yandex.ru>
2019-02-10 15:23:07 +0300

    message

--- src/main/java/inno/l7/homework/MethodBuilder.java
+++ src/main/java/inno/l7/homework/MethodBuilder.java
@@ -96,7 +96,7 @@ public class MethodBuilder {
     public static void main(String[] args) {
 
         if (args.length!=2) {
-            System.out.println("Wrong number of args");
+            System.out.println("Wrong number of args");//
             return;
         }
 
